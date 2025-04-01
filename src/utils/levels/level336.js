cff438302e081f95ea2ab19b13e3275596123b59
ThreeLetterShift <adampyro@gmail.com>
2025-04-01 05:44:07 -0700

    Create level336.js
    Funny April Fools joke, but still solvable.

---
+++ src/utils/levels/level336.js
@@ -0,0 +1,39 @@
+import {
+  altoMain,
+  anakiwa,
+  bittersweet,
+  celadon,
+  chardonnay,
+  lightOrchid,
+  lightWisteria,
+  nomad,
+  saharaSand,
+} from "../colors";
+
+const level336 = {
+  size: 9,
+  colorRegions: [
+    ["E", "E", "E", "E", "E", "B", "E", "E", "E"],
+    ["E", "E", "E", "E", "E", "E", "E", "F", "E"],
+    ["E", "E", "C", "E", "E", "E", "E", "E", "E"],
+    ["H", "E", "E", "E", "A", "E", "E", "E", "E"],
+    ["E", "E", "E", "E", "E", "E", "D", "E", "E"],
+    ["E", "E", "E", "E", "E", "E", "E", "E", "A"],
+    ["E", "E", "E", "E", "E", "B", "E", "E", "E"],
+    ["E", "E", "E", "G", "E", "E", "E", "E", "E"],
+    ["E", "I", "E", "E", "E", "E", "E", "E", "E"],
+  ],
+  regionColors: {
+    A: lightWisteria,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+    H: nomad,
+    I: lightOrchid,
+  },
+};
+
+export default level336;
